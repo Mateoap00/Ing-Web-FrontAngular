c70bf5430cd96af7ce4b4fd712014c93556ba7b1
mateoap00 <mateo.a.p.2000@gmail.com>
2024-10-10 10:42:14 -0500

    Actualiza nombre de carpeta Environments

--- src/app/Servicios/estado.service.ts
+++ src/app/Servicios/estado.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+import { environment } from '../../Environments/environment';
 import { Observable } from 'rxjs';
 import { Estado } from '../Interfaces/estado';
 
